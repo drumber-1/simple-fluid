e8b2df306c40c374d7d46e4d43f18920ed48baae
drumber-1 <drumber01@gmail.com>
2015-05-19 15:07:45 +0100

    Added initializer to grid

--- src/Grid.hpp
+++ src/Grid.hpp
@@ -6,6 +6,7 @@
 
 #include "util/Array2.hpp"
 #include "FluidVariable.hpp"
+#include "boundary.hpp"
 
 
 template <size_t NX, size_t NY>
@@ -19,10 +20,13 @@ private:
 
 	const double mSpacing;
 	std::array<FluidArray, FluidVariable::NVAR> mFluid;
+
+    void initialise();
 };
 
 template <size_t NX, size_t NY>
 Grid<NX, NY>::Grid(double spacing) : mSpacing(spacing) {
+    initialise();
 }
 
 template <size_t NX, size_t NY>
@@ -42,5 +46,27 @@ void Grid<NX, NY>::print_var(FluidVariable var, const std::string& postfix) {
         for (int j = 0; j < NY; ++j) {
             out << i * mSpacing << "\t" << j * mSpacing << "\t" << mFluid[var](i, j) << "\n";
         }
+    }
+}
+
+template <size_t NX, size_t NY>
+void Grid<NX, NY>::initialise() {
+    for (int i = 0; i < NX; ++i) {
+        for (int j = 0; j < NY; ++j) {
+            mFluid[FluidVariable::DENSITY](i, j) = 1.0;
+            mFluid[FluidVariable::VELOCITY_X](i, j) = 1.0;
+            mFluid[FluidVariable::VELOCITY_Y](i, j) = 0.0;
+            mFluid[FluidVariable::BUFFER_A](i, j) = 0.0;
+            mFluid[FluidVariable::BUFFER_B](i, j) = 0.0;
+
+            if (i > NX / 4 && i < 3 * NX / 4 && j > NY / 4 && j < 3 * NY / 4) {
+                mFluid[FluidVariable::DENSITY](i, j) = 10.0;
+            }
+        }
+    }
+
+    for (int n = 0; n < FluidVariable::NVAR; ++n) {
+        FluidVariable var = static_cast<FluidVariable>(n);
+        set_bounds_wall(var, mFluid[var]);
     }
 }
