4e01ac3bbae8bf6c328f16afbe4e424884bc6fd8
drumber-1 <drumber01@gmail.com>
2015-05-19 14:32:40 +0100

    Added simple text output

--- src/Grid.hpp
+++ src/Grid.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <fstream>
 #include <array>
+#include <string>
 
 #include "util/Array2.hpp"
 #include "FluidVariable.hpp"
@@ -11,6 +13,7 @@ class Grid {
 public:
 	Grid(double spacing);
 	void step(double dt);
+    void print_var(FluidVariable var, const std::string& postfix);
 private:
     using FluidArray = Array2<double, NX, NY>;
 
@@ -24,4 +27,20 @@ Grid<NX, NY>::Grid(double spacing) : mSpacing(spacing) {
 
 template <size_t NX, size_t NY>
 void Grid<NX, NY>::step(double dt) {
+}
+
+template <size_t NX, size_t NY>
+void Grid<NX, NY>::print_var(FluidVariable var, const std::string& postfix) {
+    std::string fname = VARNAMES[var];
+    if (postfix != "") {
+        fname += "_" + postfix;
+    }
+    fname += ".txt";
+    std::ofstream out(fname.c_str());
+    out <<"X\tY\t" << VARNAMES[var] << "\n";
+    for (int i = 0; i < NX; ++i) {
+        for (int j = 0; j < NY; ++j) {
+            out << i * mSpacing << "\t" << j * mSpacing << "\t" << mFluid[var](i, j) << "\n";
+        }
+    }
 }
