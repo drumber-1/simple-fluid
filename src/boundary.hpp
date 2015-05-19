e392c71f406cf14bfa567f993ddb0e4c89d2b643
drumber-1 <drumber01@gmail.com>
2015-05-19 15:07:10 +0100

    Fixed reference

--- src/boundary.hpp
+++ src/boundary.hpp
@@ -7,14 +7,14 @@ template <size_t NX, size_t NY>
 using FluidArray = Array2<double, NX, NY>;
 
 template <size_t NX, size_t NY>
-void set_bounds_wall(FluidVariable var, FluidArray<NX, NY> array) {
+void set_bounds_wall(FluidVariable var, FluidArray<NX, NY>& array) {
     for (size_t i = 1; i < NX - 1; ++i) {
         array(i, 0     ) = (var == FluidVariable::VELOCITY_Y) ? -array(i, 1     ) : array(i, 1     );
         array(i, NY - 1) = (var == FluidVariable::VELOCITY_Y) ? -array(i, NY - 2) : array(i, NY - 2);
     }
     for (size_t j = 1; j < NY - 1; ++j) {
-        array(0     , j) = var == (FluidVariable::VELOCITY_X) ? -array(0     , j) : array(0     , j);
-        array(NX - 1, j) = var == (FluidVariable::VELOCITY_X) ? -array(NX - 2, j) : array(NX - 2, j);
+        array(0     , j) = (var == (FluidVariable::VELOCITY_X)) ? -array(1     , j) : array(1     , j);
+        array(NX - 1, j) = (var == (FluidVariable::VELOCITY_X)) ? -array(NX - 2, j) : array(NX - 2, j);
     }
     array(0     , 0     ) = 0.5 * (array(1     , 0     ) + array(0     , 1     ));
     array(0     , NY - 1) = 0.5 * (array(1     , NY - 1) + array(0     , NY - 2));
