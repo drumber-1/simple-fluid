7fe632dcf7c21b6bdd6ef25dfaf2b0a4a014e80b
drumber-1 <drumber01@gmail.com>
2015-07-07 15:06:14 +0100

    Moved renderer to own class

---
+++ src/graphics/GridRenderer.hpp
@@ -0,0 +1,176 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "../fluid/Grid.hpp"
+
+template <size_t NX, size_t NY>
+class GridRenderer {
+public:
+    GridRenderer(std::string colour_map,
+                 float grid_scale,
+                 float density_scale,
+                 float velocity_scale);
+
+    void initialiseVertices(const Grid<NX, NY> &grid);
+    void initialiseDensityVertices(const Grid<NX, NY> &grid);
+    void initialiseVelocityVertices(const Grid<NX, NY> &grid);
+    void initialiseGridVertices(const Grid<NX, NY> &grid);
+
+    void updateVertices(const Grid<NX, NY> &grid);
+    void updateDensityVertices(const Grid<NX, NY> &grid);
+    void updateVelocityVertices(const Grid<NX, NY> &grid);
+
+    void render(sf::RenderWindow& window);
+
+    bool draw_velocity;
+    bool draw_grid;
+private:
+    sf::VertexArray vertices_density;
+    sf::VertexArray vertices_velocity;
+    sf::VertexArray vertices_grid;
+
+    sf::Texture colour_map;
+
+    float pixels_per_cell;
+    float scale_density;
+    float scale_velocity;
+
+    const sf::Color color_velocity = sf::Color::Red;
+    const sf::Color color_grid = sf::Color::Blue;
+};
+
+template <size_t NX,size_t NY>
+GridRenderer<NX, NY>::GridRenderer(std::string cmap,
+                                   float pixels_per_cell,
+                                   float density_scale,
+                                   float velocity_scale) :
+        draw_velocity(false),
+        draw_grid(false),
+        pixels_per_cell(pixels_per_cell),
+        scale_density(density_scale),
+        scale_velocity(velocity_scale) {
+    colour_map.loadFromFile(cmap);
+
+    vertices_density.setPrimitiveType(sf::Quads);
+    vertices_density.resize((unsigned int)(NX * NY * 4));
+
+    vertices_velocity.setPrimitiveType(sf::Lines);
+    vertices_velocity.resize((unsigned int)(NX * NY * 2));
+
+    vertices_grid.setPrimitiveType(sf::Lines);
+    vertices_grid.resize((unsigned int)(2 * (NX + NY + 2)));
+};
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::initialiseVertices(const Grid<NX, NY> &grid) {
+    initialiseDensityVertices(grid);
+    initialiseGridVertices(grid);
+    initialiseVelocityVertices(grid);
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::initialiseDensityVertices(const Grid<NX, NY> &grid) {
+    for (size_t i = 0; i < NX; ++i) {
+        for (size_t j = 0; j < NY; ++j) {
+            sf::Vertex* quad = &vertices_density[(i + j * NX) * 4];
+
+            quad[0].position = sf::Vector2f(pixels_per_cell *  i     , pixels_per_cell * (j    ));
+            quad[1].position = sf::Vector2f(pixels_per_cell * (i + 1), pixels_per_cell * (j    ));
+            quad[2].position = sf::Vector2f(pixels_per_cell * (i + 1), pixels_per_cell * (j + 1));
+            quad[3].position = sf::Vector2f(pixels_per_cell * (i    ), pixels_per_cell * (j + 1));
+        }
+    }
+    updateDensityVertices(grid);
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::initialiseVelocityVertices(const Grid<NX, NY> &grid) {
+    for (size_t i = 0; i < NX; ++i) {
+        for (size_t j = 0; j < NY; ++j) {
+            sf::Vertex* line = &vertices_velocity[(i + j * NX) * 2];
+
+            float xstart = pixels_per_cell * (i + 0.5f);
+            float ystart = pixels_per_cell * (j + 0.5f);
+
+            line[0].position = sf::Vector2f(xstart, ystart);
+
+            line[0].color = color_velocity;
+            line[1].color = color_velocity;
+        }
+    }
+    updateVelocityVertices(grid);
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::initialiseGridVertices(const Grid<NX, NY> &grid) {
+    for (size_t i = 0; i <= NX; ++i) {
+        sf::Vertex* line = &vertices_grid[2 * i];
+
+        line[0].position = sf::Vector2f(i * pixels_per_cell, 0);
+        line[1].position = sf::Vector2f(i * pixels_per_cell, NY * pixels_per_cell);
+
+        line[0].color = color_grid;
+        line[1].color = color_grid;
+    }
+    for (size_t j = 0; j <= NY; ++j) {
+        sf::Vertex* line = &vertices_grid[2 * (NX + 1 + j)];
+
+        line[0].position = sf::Vector2f(0, j * pixels_per_cell);
+        line[1].position = sf::Vector2f(NX * pixels_per_cell, j * pixels_per_cell);
+
+        line[0].color = color_grid;
+        line[1].color = color_grid;
+    }
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::updateVertices(const Grid<NX, NY> &grid) {
+    updateDensityVertices(grid);
+    updateVelocityVertices(grid);
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::updateDensityVertices(const Grid<NX, NY> &grid) {
+    int cmap_size = colour_map.getSize().x;
+
+    for (size_t i = 0; i < NX; ++i) {
+        for (size_t j = 0; j < NY; ++j) {
+            sf::Vertex* quad = &vertices_density[(i + j * NX) * 4];
+
+            int col_x = (int)(cmap_size * grid.density(i, j) / scale_density);
+
+            for (int q = 0; q < 4; ++q) {
+                quad[q].texCoords = sf::Vector2f(col_x, 0);
+            }
+        }
+    }
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::updateVelocityVertices(const Grid<NX, NY> &grid) {
+    for (size_t i = 0; i < NX; ++i) {
+        for (size_t j = 0; j < NY; ++j) {
+            sf::Vertex* line = &vertices_velocity[(i + j * NX) * 2];
+
+            float xstart = pixels_per_cell * (i + 0.5f);
+            float ystart = pixels_per_cell * (j + 0.5f);
+
+            float xoffset = pixels_per_cell *  grid.velocity_x(i, j) * scale_velocity;
+            float yoffset = pixels_per_cell *  grid.velocity_y(i, j) * scale_velocity;
+
+            line[0].position = sf::Vector2f(xstart, ystart);
+            line[1].position = sf::Vector2f(xstart + xoffset, ystart + yoffset);;
+        }
+    }
+}
+
+template <size_t NX, size_t NY>
+void GridRenderer<NX, NY>::render(sf::RenderWindow &window) {
+    window.draw(vertices_density, &colour_map);
+    if (draw_velocity) {
+        window.draw(vertices_velocity);
+    }
+    if (draw_grid) {
+        window.draw(vertices_grid);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,38 @@
 #include <iostream>
 #include <cfenv>
 
-#include "fluid/Grid.hpp"
 #include <SFML/Graphics.hpp>
+#include "fluid/Grid.hpp"
+#include "graphics/GridRenderer.hpp"
 
 const size_t GRIDX = 100;
 const size_t GRIDY = 100;
 
-const float RHOMAX = 10.0;
-const float GRID_SCALE = 5.0f;
+const float PIXELS_PER_CELL = 5.0f;
 
-template <size_t NX, size_t NY>
-void updateDensityVertices(const Grid<NX, NY> &grid, sf::VertexArray &vertices) {
-
-    for (size_t i = 0; i < GRIDX; ++i) {
-        for (size_t j = 0; j < GRIDY; ++j) {
-            sf::Vertex* quad = &vertices[(i + j * GRIDX) * 4];
-
-            int colx = (int)(256 * grid.density(i, j) / RHOMAX);
-            int coly = 0;
-
-            quad[0].position = sf::Vector2f(GRID_SCALE *  i     , GRID_SCALE * (j    ));
-            quad[1].position = sf::Vector2f(GRID_SCALE * (i + 1), GRID_SCALE * (j    ));
-            quad[2].position = sf::Vector2f(GRID_SCALE * (i + 1), GRID_SCALE * (j + 1));
-            quad[3].position = sf::Vector2f(GRID_SCALE * (i    ), GRID_SCALE * (j + 1));
-
-            for (int q = 0; q < 4; ++q) {
-                quad[q].texCoords = sf::Vector2f(colx, coly);
-            }
-        }
-    }
-}
-
-template <size_t NX, size_t NY>
-void updateVelocityVertices(const Grid<NX, NY> &grid, sf::VertexArray &vertices) {
-    float velocity_scaling = 0.1f;
-
-    for (size_t i = 0; i < GRIDX; ++i) {
-        for (size_t j = 0; j < GRIDY; ++j) {
-            sf::Vertex* line = &vertices[(i + j * GRIDX) * 2];
-
-            float xstart = GRID_SCALE * (i + 0.5f);
-            float ystart = GRID_SCALE * (j + 0.5f);
-
-            float xoffset = GRID_SCALE *  grid.velocity_x(i, j) * velocity_scaling;
-            float yoffset = GRID_SCALE *  grid.velocity_y(i, j) * velocity_scaling;
-
-            line[0].position = sf::Vector2f(xstart, ystart);
-            line[1].position = sf::Vector2f(xstart + xoffset, ystart + yoffset);
-
-            line[0].color = sf::Color::Red;
-            line[1].color = sf::Color::Red;
-        }
-    }
-}
-
-template <size_t NX, size_t NY>
-void updateGridVertices(const Grid<NX, NY> &grid, sf::VertexArray &vertices) {
-    for (size_t i = 0; i <= GRIDX; ++i) {
-        sf::Vertex* line = &vertices[2 * i];
-
-        line[0].position = sf::Vector2f(i * GRID_SCALE, 0);
-        line[1].position = sf::Vector2f(i * GRID_SCALE, GRIDY * GRID_SCALE);
-
-        line[0].color = sf::Color::Blue;
-        line[1].color = sf::Color::Blue;
-    }
-    for (size_t j = 0; j <= GRIDY; ++j) {
-        sf::Vertex* line = &vertices[2 * (GRIDX + 1 + j)];
-
-        line[0].position = sf::Vector2f(0, j * GRID_SCALE);
-        line[1].position = sf::Vector2f(GRIDX * GRID_SCALE, j * GRID_SCALE);
-
-        line[0].color = sf::Color::Blue;
-        line[1].color = sf::Color::Blue;
-    }
-}
+const float SCALE_RHO = 10.0;
+const float SCALE_VELOCITY = 1.0f;
 
 int main() {
     const float spacing = 1.0;
-    const float force = 5000000.0f;
-    const float source = 100.0f;
+    const float force = 50000.0f;
+    const float source = 1000.0f;
 
 	Grid<GRIDX, GRIDY> grid(spacing);
 
-    sf::Texture colourMap;
-    colourMap.loadFromFile("res/colourmap.png");
-    std::cout << "Colour map loaded, (" << colourMap.getSize().x << ", " << colourMap.getSize().y << ")\n";
+    GridRenderer<GRIDX, GRIDY> grid_renderer("res/colourmap.png", PIXELS_PER_CELL, SCALE_RHO, SCALE_VELOCITY);
+    grid_renderer.initialiseVertices(grid);
 
-    sf::VertexArray densityVertices;
-    densityVertices.setPrimitiveType(sf::Quads);
-    densityVertices.resize(GRIDX * GRIDY * 4);
-    updateDensityVertices(grid, densityVertices);
+    //sf::Texture colourMap;
+    //colourMap.loadFromFile("res/colourmap.png");
+    //std::cout << "Colour map loaded, (" << colourMap.getSize().x << ", " << colourMap.getSize().y << ")\n";
 
-    sf::VertexArray velocityVertices;
-    velocityVertices.setPrimitiveType(sf::Lines);
-    velocityVertices.resize(GRIDX * GRIDY * 2);
-    updateVelocityVertices(grid, velocityVertices);
-
-    sf::VertexArray gridVertices;
-    gridVertices.setPrimitiveType(sf::Lines);
-    gridVertices.resize(2 * (GRIDX + GRIDY + 2));
-    updateGridVertices(grid, gridVertices);
-
-    unsigned int window_width = std::ceil(GRIDX * GRID_SCALE);
-    unsigned int window_height = std::ceil(GRIDY * GRID_SCALE);
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML TEST");
+    unsigned int window_width = std::ceil(GRIDX * PIXELS_PER_CELL);
+    unsigned int window_height = std::ceil(GRIDY * PIXELS_PER_CELL);
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Simple Fluid");
 
     sf::Clock clock;
     bool fluidPaused = false;
-    bool showVelocity = false;
-    bool showGrid = false;
 
     bool mouseleftdown = false;
     bool mouserightdown = false;
@@ -125,13 +47,10 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             } else if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::S) {
+                if (event.key.code == sf::Keyboard::S && fluidPaused) {
                     std::cout << "Updating grid" << "\n";
                     grid.step(0.4);
-                    updateDensityVertices(grid, densityVertices);
-                    if (showVelocity) {
-                        updateVelocityVertices(grid, velocityVertices);
-                    }
+                    grid_renderer.updateVertices(grid);
                 } else if (event.key.code == sf::Keyboard::Space) {
                     if (fluidPaused) {
                         std::cout << "Fluid unpaused\n";
@@ -142,20 +61,20 @@ int main() {
                         fluidPaused = true;
                     }
                 } else if (event.key.code == sf::Keyboard::V) {
-                    if (showVelocity) {
+                    if (grid_renderer.draw_velocity) {
                         std::cout << "Velocity lines disabled\n";
-                        showVelocity = false;
+                        grid_renderer.draw_velocity = false;
                     } else {
                         std::cout << "Velocity lines enabled\n";
-                        showVelocity = true;
+                        grid_renderer.draw_velocity = true;
                     }
                 } else if (event.key.code == sf::Keyboard::G) {
-                    if (showGrid) {
+                    if (grid_renderer.draw_grid) {
                         std::cout << "Grid lines disabled\n";
-                        showGrid = false;
+                        grid_renderer.draw_grid = false;
                     } else {
                         std::cout << "Grid lines enabled\n";
-                        showGrid = true;
+                        grid_renderer.draw_grid = true;
                     }
                 }
             } else if (event.type == sf::Event::MouseButtonPressed) {
@@ -181,12 +100,12 @@ int main() {
         sf::Time elapsed = clock.restart();
 
         if (!fluidPaused) {
-            size_t i = size_t(mousex / GRID_SCALE);
-            size_t j = size_t(mousey / GRID_SCALE);
+            size_t i = size_t(mousex / PIXELS_PER_CELL);
+            size_t j = size_t(mousey / PIXELS_PER_CELL);
 
             if (mouseleftdown) {
-                grid.source_velocity_x(i, j) = force * (mousex - mousedownx) / GRID_SCALE;
-                grid.source_velocity_y(i, j) = force * (mousey - mousedowny) / GRID_SCALE;
+                grid.source_velocity_x(i, j) = force * (mousex - mousedownx) / PIXELS_PER_CELL;
+                grid.source_velocity_y(i, j) = force * (mousey - mousedowny) / PIXELS_PER_CELL;
                 mousedownx = mousex;
                 mousedowny = mousey;
             }
@@ -196,20 +115,11 @@ int main() {
 
             float dt = elapsed.asSeconds();
             grid.step(dt);
-            updateDensityVertices(grid, densityVertices);
-            if (showVelocity) {
-                updateVelocityVertices(grid, velocityVertices);
-            }
+            grid_renderer.updateVertices(grid);
         }
 
         window.clear();
-        window.draw(densityVertices, &colourMap);
-        if (showVelocity) {
-            window.draw(velocityVertices);
-        }
-        if (showGrid) {
-            window.draw(gridVertices);
-        }
+        grid_renderer.render(window);
         window.display();
     }
 
