5c8bde2be068c841719fd46c1c3dc74d2e5642ee
drumber-1 <drumber01@gmail.com>
2015-07-09 23:39:43 +0100

    Changed data structure from std::array to std::vector so large grids will not cause stack overflow

--- src/fluid/util/Array2.hpp
+++ src/fluid/util/Array2.hpp
@@ -1,26 +1,31 @@
 #pragma once
 
-#include <array>
+#include <vector>
 
 template <typename T, size_t NX, size_t NY>
 class Array2 {
 public:
+	Array2();
+
 	T operator() (size_t i, size_t j) const;
 	T& operator() (size_t i, size_t j);
 	inline size_t flattened_index(size_t i, size_t j) const;
 private:
-	std::array<T, NX*NY> mData;
+	std::vector<T> data;
 };
 
+template <typename T, size_t NX, size_t NY>
+Array2<T, NX, NY>::Array2() : data(NX * NY) {
+}
 
 template <typename T, size_t NX, size_t NY>
 T Array2<T, NX, NY>::operator() (size_t i, size_t j) const {
-	return mData[flattened_index(i, j)];
+	return data[flattened_index(i, j)];
 }
 
 template <typename T, size_t NX, size_t NY>
 T& Array2<T, NX, NY>::operator() (size_t i, size_t j) {
-	return mData[flattened_index(i, j)];
+	return data[flattened_index(i, j)];
 }
 
 template <typename T, size_t NX, size_t NY>
